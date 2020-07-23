d2ddb7ee99da20ab8c0092b0c68ac01f6ca0436d
Rahul Tah <tahrahul.tah@gmail.com>
2020-07-23 15:43:32 +0530

    Remove old files

---
+++ bundle.js
@@ -0,0 +1,49 @@
+console.log("connected")
+
+
+        let movies = [];
+        const addMovie = (ev)=>{
+            ev.preventDefault();  //to stop the form submitting
+            let movie = {
+                id: Date.now(),
+                title: document.getElementById('title').value,
+                year: document.getElementById('yr').value
+            }
+            movies.push(movie);
+            document.forms[0].reset(); // to clear the form for the next entries
+            //document.querySelector('form').reset();
+            buildTable(movies)
+
+            function buildTable(data){
+                var table = document.getElementById('myTable')
+                for (var i = 0; i < data.length; i++){
+                    var row = `<tr>
+                                    <td>${data[i].title}</td>
+                                    <td>${data[i].year}</td>
+                                    
+                              </tr>`
+                    table.innerHTML += row
+        
+        
+                }
+            }
+
+
+            //for display purposes only
+            console.warn('added' , {movies} );
+            let pre = document.querySelector('#msg pre');
+            pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
+
+             
+
+
+        }
+        let data = JSON.stringify(movies);  
+
+       
+{
+    console.log('success');
+}
+        document.addEventListener('DOMContentLoaded', ()=>{
+            document.getElementById('btn').addEventListener('click', addMovie);
+        });
